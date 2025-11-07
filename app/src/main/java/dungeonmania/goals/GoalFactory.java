c1c44931e34e0c635da04b4c6db1fcfe85d90a37
gabi <z5592426@ad.unsw.edu.au>
2025-11-07 19:46:52 +1100

    Refactored to goals to comply with OCP

--- app/src/main/java/dungeonmania/goals/GoalFactory.java
+++ app/src/main/java/dungeonmania/goals/GoalFactory.java
@@ -3,27 +3,33 @@ package dungeonmania.goals;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import dungeonmania.goals.composites.AndGoal;
+import dungeonmania.goals.composites.OrGoal;
+import dungeonmania.goals.leaves.BoulderGoal;
+import dungeonmania.goals.leaves.ExitGoal;
+import dungeonmania.goals.leaves.TreasureGoal;
+
 public class GoalFactory {
     public static Goal createGoal(JSONObject jsonGoal, JSONObject config) {
         JSONArray subgoals;
         return switch (jsonGoal.getString("goal")) {
-            case "AND" -> {
-                subgoals = jsonGoal.getJSONArray("subgoals");
-                yield new Goal("AND", createGoal(subgoals.getJSONObject(0), config),
-                        createGoal(subgoals.getJSONObject(1), config));
-            }
-            case "OR" -> {
-                subgoals = jsonGoal.getJSONArray("subgoals");
-                yield new Goal("OR", createGoal(subgoals.getJSONObject(0), config),
-                        createGoal(subgoals.getJSONObject(1), config));
-            }
-            case "exit" -> new Goal("exit");
-            case "boulders" -> new Goal("boulders");
-            case "treasure" -> {
-                int treasureGoal = config.optInt("treasure_goal", 1);
-                yield new Goal("treasure", treasureGoal);
-            }
-            default -> null;
+        case "AND" -> {
+            subgoals = jsonGoal.getJSONArray("subgoals");
+            yield new AndGoal(createGoal(subgoals.getJSONObject(0), config),
+                    createGoal(subgoals.getJSONObject(1), config));
+        }
+        case "OR" -> {
+            subgoals = jsonGoal.getJSONArray("subgoals");
+            yield new OrGoal(createGoal(subgoals.getJSONObject(0), config),
+                    createGoal(subgoals.getJSONObject(1), config));
+        }
+        case "exit" -> new ExitGoal();
+        case "boulders" -> new BoulderGoal();
+        case "treasure" -> {
+            int treasureGoal = config.optInt("treasure_goal", 1);
+            yield new TreasureGoal(treasureGoal);
+        }
+        default -> null;
         };
     }
 }
---
+++ app/src/main/java/dungeonmania/goals/leaves/BoulderGoal.java
@@ -0,0 +1,18 @@
+package dungeonmania.goals.leaves;
+
+import dungeonmania.Game;
+import dungeonmania.entities.Switch;
+import dungeonmania.goals.Goal;
+
+public class BoulderGoal implements Goal {
+    @Override
+    public boolean achieved(Game game) {
+        return game.getMap().getEntities(Switch.class).stream().allMatch(s -> s.isActivated());
+    }
+
+    @Override
+    public String toString(Game game) {
+        return achieved(game) ? "" : ":boulders";
+    }
+
+}
